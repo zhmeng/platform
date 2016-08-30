49c8ceb256a452ffd4ab779030c57f0da1b69ce2
zhmeng <ccsoul@yeah.net>
2016-08-30 17:38:35 +0800

    delete models

--- plugins/backend/app/controllers/backend/IndexAction.java
+++ plugins/backend/app/controllers/backend/IndexAction.java
@@ -1,25 +1,10 @@
 package controllers.backend;
 
-import baser.aspect.LogTimeInterceptor;
-import baser.aspect.With;
-import com.avaje.ebean.Page;
-import com.google.common.collect.Maps;
-import forms.BankForm;
-import models.Bank;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import play.Logger;
-import play.data.Form;
-import play.libs.F;
-import play.libs.Json;
-import play.libs.WS;
 import play.mvc.Controller;
 import play.mvc.Result;
-import plugins.ebean.Paging;
-import services.backend.BankService;
 
-import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static plugins.freemarker.Freemarker._;
@@ -30,9 +15,6 @@ import static plugins.freemarker.Freemarker.view;
  */
 @Service
 public class IndexAction extends Controller {
-
-    @Autowired
-    private BankService bankService;
 
     public AtomicInteger ai = new AtomicInteger(0);
 
@@ -45,9 +27,5 @@ public class IndexAction extends Controller {
         return ok(view("backend/index.ftl", _("title", "PLATFORM")));
     }
 
-    public Result demoData(){
-        BankForm bankForm = Form.form(BankForm.class).bindFromRequest().get();
-        Page<Bank> banks = bankService.list(bankForm);
-        return ok(Paging.toPage(banks).toJson());
-    }
+
 }
